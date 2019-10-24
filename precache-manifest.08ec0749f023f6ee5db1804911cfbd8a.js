f9f82920914eaab65d9d7e1cd2f999e4536321ed
pyankovit <pyankov.it@gmail.com>
2019-10-24 18:43:56 +0300

    Updates

---
+++ precache-manifest.08ec0749f023f6ee5db1804911cfbd8a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "337962559af216e7605ea55dbbe2b8d7",
+    "url": "/movie-api-hooks/index.html"
+  },
+  {
+    "revision": "873e890ea736f7640e6b",
+    "url": "/movie-api-hooks/static/css/main.739aba6a.chunk.css"
+  },
+  {
+    "revision": "71d9ae145a3271043a2d",
+    "url": "/movie-api-hooks/static/js/2.e7e13de1.chunk.js"
+  },
+  {
+    "revision": "873e890ea736f7640e6b",
+    "url": "/movie-api-hooks/static/js/main.64efaf64.chunk.js"
+  },
+  {
+    "revision": "ef6255a843b0edbee14a",
+    "url": "/movie-api-hooks/static/js/runtime-main.4719636b.js"
+  }
+]);
